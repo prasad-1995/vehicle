7b71ac4137bebcd77d2fcf639d4b0ef55d5003ba
prasad-1995 <prasadkane9@gmail.com>
2021-11-17 15:49:42 +0530

    commit

---
+++ cars/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from cars import views
+
+urlpatterns = [
+    path('', views.cars, name='cars'),
+    path('<int:id>', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search'),
+]
